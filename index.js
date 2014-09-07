74853f77426cc273937615d085a1bd6733a381e1
Flavio Del Bianco <info@flaviodelbianco.com>
2014-09-08 00:36:22 +0200

    Added id_grants and id_output to the Schema options

--- index.js
+++ index.js
@@ -5,27 +5,47 @@ var format_fields_plugin = function (schema, options) {
 
     var getGrants = function (options) {
         var requested_grants = (options.grants) ? options.grants: null;
-        if (!util.isArray(requested_grants)) {
+        if (!util.isArray(requested_grants) && requested_grants != null) {
             requested_grants = [requested_grants];
         }
 
         return requested_grants;
     }
 
-    var checkFields = function (paths, entity, grants) {
-        var output = {};
+    var isAllowed = function (requested_grants, grants) {
+        for (var n = 0; n < grants.length; n++) {
+            if (requested_grants.indexOf(grants[n]) >= 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    var checkFields = function (schema, entity, grants) {
+        var output = {},
+            paths = schema.paths;
+
+        if (schema.options.id_grants) {
+            var requested_grants = (schema.options.id_grants) ? schema.options.id_grants: null;
+            if (!util.isArray(requested_grants) && requested_grants != null) {
+                requested_grants = [requested_grants];
+            }
+            if (requested_grants && isAllowed(requested_grants, grants)) {
+                if (schema.options.id_output) {
+                    output[schema.options.id_output] = entity['_id'];
+                } else {
+                    output['_id'] = entity['_id'];
+                }
+            }
+        }
         Object.keys(paths).forEach(function (name) {
             if (paths[name].schema) {
                 var requested_grants = getGrants(paths[name].schema.options);
             } else {
                 var requested_grants = getGrants(paths[name].options);
             }
-
-            for (var n = 0; n < grants.length; n++) {
-                if (requested_grants.indexOf(grants[n]) >= 0) {
-                    output[name] = entity[name]
-                    break;
-                }
+            if (requested_grants && isAllowed(requested_grants, grants)) {
+                output[name] = entity[name]
             }
         });
 
@@ -39,7 +59,7 @@ var format_fields_plugin = function (schema, options) {
 
         grants.push("public");
 
-        return checkFields(schema.paths, entity, grants);
+        return checkFields(schema, entity, grants);
     });
 
     schema.methods.format = function (grants) {
