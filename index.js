11b08cafbc51c1e249b3d9d159b82b4a20288014
Flavio Del Bianco <info@flaviodelbianco.com>
2017-03-25 07:26:20 +0100

    Implemented dot-object to better manage objects

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 var util = require('util'),
-    extend = require('extend');
+    extend = require('extend'),
+    dot = require('dot-object');
 
-var format_fields_plugin = function (schema, options) {
-
+var format_fields_plugin = function (schema) {
     schema.grantSchema = {};
 
     var getGrantsFromOptions = function (name, options) {
+        var requested_grants;
         if (options.grants) {
-            var requested_grants = options.grants;
+            requested_grants = options.grants;
         } else if (options.type && options.type[0] && options.type[0].grants) {
-            var requested_grants = options.type[0].grants;
+            requested_grants = options.type[0].grants;
         } else {
-            var requested_grants = null;
+            requested_grants = null;
         }
 
-        if (!util.isArray(requested_grants) && requested_grants != null) {
+        if (requested_grants && !util.isArray(requested_grants)) {
             requested_grants = [requested_grants];
         }
 
@@ -22,11 +23,11 @@ var format_fields_plugin = function (schema, options) {
     };
 
     for (var name in schema.paths) {
-        var options = schema.paths[name].options;
+        var opt = schema.paths[name].options;
         if (schema.paths[name].schema) {
-            options = schema.paths[name].schema.options;
+            opt = schema.paths[name].schema.options;
         }
-        schema.grantSchema[name] = getGrantsFromOptions(name, options);
+        schema.grantSchema[name] = getGrantsFromOptions(name, opt);
     }
 
     var isAllowed = function (requested_grants, grants) {
@@ -40,74 +41,29 @@ var format_fields_plugin = function (schema, options) {
 
     var checkSchema = function (schema, entity, grants) {
         var output = {},
-            paths = schema.paths;
+            obj = extend({}, entity.toObject()),
+            paths = schema.paths,
+            requested_grants;
 
         if (schema.options.id_grants) {
-            var requested_grants = (schema.options.id_grants) ? schema.options.id_grants: null;
-            if (!util.isArray(requested_grants) && requested_grants != null) {
+            requested_grants = (schema.options.id_grants) ? schema.options.id_grants: null;
+            if (requested_grants && !util.isArray(requested_grants)) {
                 requested_grants = [requested_grants];
             }
             if (requested_grants && isAllowed(requested_grants, grants)) {
                 if (schema.options.id_output) {
-                    output[schema.options.id_output] = entity['_id'];
+                    output[schema.options.id_output] = obj._id;
                 } else {
-                    output['_id'] = entity['_id'];
+                    output._id = obj._id;
                 }
             }
         }
         for (var name in paths) {
-            var requested_grants = (schema.grantSchema[name]) || [];
+            requested_grants = (schema.grantSchema[name]) || [];
 
             if (requested_grants && isAllowed(requested_grants, grants)) {
-                var arrName = name.split('.');
-                var first = arrName[0];
-                var second = arrName[1];
-                var third = arrName[2];
-                var fourth = arrName[3];
-                var fifth = arrName[4];
-                if (entity[first] != undefined) {
-                    if (fifth) {
-                        if (!output[first]) {
-                            output[first] = {};
-                        }
-                        if (!output[first][second]) {
-                            output[first][second] = {};
-                        }
-                        if (!output[first][second][third]) {
-                            output[first][second][third] = {};
-                        }
-                        if (!output[first][second][third][fourth]) {
-                            output[first][second][third][fourth] = {};
-                        }
-                        output[first][second][third][fourth][fifth] = entity[first][second][third][fourth][fifth];
-                    } else if (fourth) {
-                        if (!output[first]) {
-                            output[first] = {};
-                        }
-                        if (!output[first][second]) {
-                            output[first][second] = {};
-                        }
-                        if (!output[first][second][third]) {
-                            output[first][second][third] = {};
-                        }
-                        output[first][second][third][fourth] = entity[first][second][third][fourth];
-                    } else if (third) {
-                        if (!output[first]) {
-                            output[first] = {};
-                        }
-                        if (!output[first][second]) {
-                            output[first][second] = {};
-                        }
-                        output[first][second][third] = entity[first][second][third];
-                    } else if (second) {
-                        if (!output[first]) {
-                            output[first] = {};
-                        }
-                        output[first][second] = entity[first][second];
-                    } else {
-                        output[first] = entity[first];
-                    }
-                }
+                var value = dot.pick(name, obj);
+                dot.str(name, value, output);
             }
         }
         return output;
